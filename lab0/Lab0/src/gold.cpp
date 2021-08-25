b0c403aece03c0ee0c14144e43f47db4535fe590
Sarah Huang <shuang24@tesla14.eecs.utk.edu>
2021-08-25 17:56:00 -0400

    finished the code

--- lab0/Lab0/src/gold.cpp
+++ lab0/Lab0/src/gold.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
-
 using namespace std;
 
+
 int main(){
-	char i;
+	char c;
+	int total;
+
+	//some loop(cin >> c)
+	//Check if not '-' or '.'
+	//Add to running total based on character value
+	
+	while(cin >> c){
+		if(c != '.'  || c != '-'){
+			if(c >= 'A' && c <= 'Z'){
+				total += (c - 'A') + 1;
+			}
+		}
+	}
+
+
+
+	cout << total << endl;
+	
 	return 0;
 }
