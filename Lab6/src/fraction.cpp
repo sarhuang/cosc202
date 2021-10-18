a6a237731a8c1070ba044689a80305c5e0c43550
Sarah Huang <shuang24@hydra2.eecs.utk.edu>
2021-10-17 20:02:48 -0400

    final

--- Lab6/src/fraction.cpp
+++ Lab6/src/fraction.cpp
@@ -1,8 +1,9 @@
-/* Name:
- * Date:
- * Program:
- * Purpose:
- */
+/* Name: Sarah Huang
+ * Date: 10/17/21
+ * Program: fraction.cpp
+ * Purpose: Simulate a fraction, where the numerator and the denominator are both products of positive integers.  
+ *			Internally, you will represent the fraction as two multisets -- one for the numerator and one for the denominator.
+ *			Like fractions, you want to remove a number from the numerator and denominator if it's both there. */
 
 #include <iostream>
 #include <set>
@@ -29,7 +30,7 @@ bool Fraction::Multiply_Number(int n){
 		if(denominator.find(n) == denominator.end())
 			numerator.insert(n);
 		else
-			denominator.erase(denominator.lower_bound(n));
+			denominator.erase(denominator.lower_bound(n));	//Only removes one copy of the number 
 	}
 	return true;
 }
@@ -59,7 +60,6 @@ bool Fraction::Multiply_Factorial(int n){
 				numerator.insert(i);
 			else
 				denominator.erase(denominator.lower_bound(i));
-				//denominator.erase(i);
 		}
 		return true;
 	}
@@ -74,7 +74,6 @@ bool Fraction::Divide_Factorial(int n){
 				denominator.insert(i);
 			else
 				numerator.erase(numerator.lower_bound(i));
-				//numerator.erase(i);
 		}
 		return true;
 	}
@@ -85,10 +84,14 @@ bool Fraction::Divide_Factorial(int n){
 
 
 //Effect multiplying by n-choose-k
+//		n!
+//	--------
+//	k! (n-k)!
 bool Fraction::Multiply_Binom(int n, int k){
 	if(n <= 0 || k < 0)
 		return false;
 	
+	//Multiply n!
 	for(int i = 2; i <= n; i++){
 		if(denominator.find(i) == denominator.end())
 			numerator.insert(i);
@@ -96,6 +99,7 @@ bool Fraction::Multiply_Binom(int n, int k){
 			denominator.erase(denominator.lower_bound(i));
 	}
 	
+	//Divide k!
 	for(int j = 2; j <= k; j++){
 		if(numerator.find(j) == numerator.end())
 			denominator.insert(j);
@@ -103,6 +107,7 @@ bool Fraction::Multiply_Binom(int n, int k){
 			numerator.erase(numerator.lower_bound(j));
 	}
 
+	//Divide (n-k)!
 	for(int m = 2; m <= (n-k); m++){
 		if(numerator.find(m) == numerator.end())
 			denominator.insert(m);
@@ -117,6 +122,7 @@ bool Fraction::Divide_Binom(int n, int k){
 	if(n <= 0 || k < 0)
 		return false;
 
+	//Multiply k!
 	for(int j = 2; j <= k; j++){
 		if(denominator.find(j) == denominator.end())
 			numerator.insert(j);
@@ -124,6 +130,7 @@ bool Fraction::Divide_Binom(int n, int k){
 			denominator.erase(denominator.lower_bound(j));
 	}
 
+	//Multiply (n-k)!
 	for(int m = 2; m <= (n-k); m++){
 		if(denominator.find(m) == denominator.end())
 			numerator.insert(m);
@@ -131,6 +138,7 @@ bool Fraction::Divide_Binom(int n, int k){
 			denominator.erase(denominator.lower_bound(m));
 	}
 
+	//Divide n!
 	for(int i = 2; i <= n; i++){
 		if(numerator.find(i) == numerator.end())
 			denominator.insert(i);
@@ -153,33 +161,36 @@ void Fraction::Print() const{
 	unsigned int signCounter = 0;
 	multiset <int>::const_iterator nit;
 
+	//If the fraction is empty
 	if(numerator.size() == 0 && denominator.size() == 0)
 		cout << 1 << endl;	
 
 	else{
+		//Print the #s in numerator
 		for(nit = numerator.begin(); nit != numerator.end(); nit++){
 			cout << *nit;
-		
-			if(signCounter < numerator.size() - 1)
-				cout << " * ";
 
+			if(signCounter < numerator.size() - 1)	//Avoid printing too many times
+				cout << " * ";
 			signCounter++;
 		}
-	
+		
 		signCounter = 0;
+		
+		//No numerator, there is no 1 in the denominator
 		if(numerator.size() == 0 && denominator.find(1) == denominator.end())
 			cout << 1 << " / ";
-		
+		//No numerator, there IS a 1 in denominator
 		if(numerator.size() != 0 && denominator.size() != 0)
 			cout << " / ";
 
 
+		//Print #s in denominator
 		for(nit = denominator.begin(); nit != denominator.end(); nit++){
 			cout << *nit;
 
 			if(signCounter < denominator.size() - 1)
 				cout << " / ";
-
 			signCounter++;
 		}
 		cout << endl;
