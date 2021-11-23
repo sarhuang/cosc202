9957229457aca3d58cab1220e9339ddb77e96d2f
Sarah Huang <shuang24@hydra2.eecs.utk.edu>
2021-11-22 21:58:11 -0500

    not done but i would cry if i lost this progress

--- LabA/src/bstree_lab.cpp
+++ LabA/src/bstree_lab.cpp
@@ -1,145 +1,254 @@
-/* Name: Sarah Huang
- * Date: 11/2
- * Program: bstree_lab.cpp
- * Purpose:
- */
-
 #include <vector>
 #include <string>
 #include <iostream>
 #include <cstdio>
+#include <set>
 #include "bstree.hpp"
 using namespace std;
-using namespace CS202::BSTree;
-using namespace CS202::BSTNode;
+using CS202::BSTree;
+using CS202::BSTNode;
 
+/* ---------------------------------------------- */
+/* You'll write all of these as part of your lab. */
 
-
-
-//Copy constructor
-BSTree::BSTree(const BSTree &t){
-	/* Dlist
-	 *	sentinel = new Dnode;
-	 *	sentinel->flink = sentinel;
-	 *	sentinel->blink = sentinel;*/
-
-
-	(void) t;
-
-	//Call assignment overload
-
-
-}
-//Assignment overload
-BSTree::BSTree& operator= (const BSTree &t){
-	/* Dlist
-	 *	Dnode* n;
-	 *	Clear();
-	 *	for(n = d.Begin(); n != d.End(); n = n->flink){
-	 *		Push_Back(n->s);
-	 *	}
-	 *	return *this;*/
-
-
-
-	//Not only do they make a copy of the tree, but the copy will be balanced
-	//In other words, the middle key will be the root of the tree. 
-	//		The middle key of the left subtree will be the key of the left child of the root
-	//		The middle key of the right subtree will be the key of the right child of the root. 
-
-	//If a tree has an even number of elements, the "middle" element is (number-of-elements)/2
-	//So, if a tree has four keys, the "middle" one is key #2 in Ordered_Keys()
-
-	(void) t;
-
-
-	//Call Ordered_Keys() to get a vector of keys in sorted order & Ordered_Vals() to get vals
-	//Call make_balanced_tree() to create subtree
-
-
-	return *this;
-}
-
-
-
-
-
-//Distance from a node to the root.  Returns -1 if the key is not in the tree.
 int BSTree::Depth(const string &key) const{
 	(void) key;
 	int distance = 0;
 
 	BSTNode *n;
-
 	n = sentinel->right;
-	while (1) {
-		if (n == sentinel) return -1;
-		if (key == n->key) return distance;
+
+	while(1){
+		if(n == sentinel) return -1;
+		if(key == n->key) return distance;
 		n = (key < n->key) ? n->left : n->right;
 		distance++;
 	}
 }
-//Returns the depth of the node with maximum depth, plus one.	
-int BSTree::Height() const{
-	//An empty tree has a height of zero. A tree with one node has a height of one.
-	//USE recursive_find_height()
-	
-	//int recursive_find_height(const BSTNode *n) const;
-	
-	return recursive_find_height(sentinel->right) + 1;
+
+int BSTree::Height() const{ 
+	int height = recursive_find_height(sentinel->right) + 1;
+	return height;
 }
 
-//Return a vector of sorted keys
 vector <string> BSTree::Ordered_Keys() const{
 	vector <string> rv;
-	
-	//USE make_key_vector()
-	
-	//make_val_vector(sentinel->right, rv);
-	
 	make_key_vector(sentinel->right, rv);
 	return rv;
 }
 
 
 
-//Helper for Height()
-int BSTree::recursive_find_height(const BSTNode *n) const{
+
+BSTree::BSTree(const BSTree &t) {
+	(void) t;
+
+	sentinel = new BSTNode;
+	sentinel->left = NULL;
+	sentinel->right = sentinel;
+	sentinel->parent = NULL;
+	sentinel->key = "---SENTINEL---";
+	sentinel->val = NULL;
+	size = 0;
+
+	*this = t;
+}
+
+BSTree& BSTree::operator= (const BSTree &t) {
+	//call Ordered_Keys()
+	//call Ordered_Vals()
+	//call make_balanced_tree()
+
+	(void) t;
+
+	Clear();
+
+	vector <string> ok = t.Ordered_Keys();
+	vector <void *> ov = t.Ordered_Vals();
+	make_balanced_tree(ok, ov, 0, t.Size());
+	return *this;
+}
+
+
+
+
+int BSTree::recursive_find_height(const BSTNode *n) const{  
 	(void) n;
-	int h = 0;
-	
-	if(n == sentinel) return h;
+	static int maxDepth = 0;
+
+	if(sentinel == n) return -1;
+
 	recursive_find_height(n->left);
-	h += Depth(n);
 	recursive_find_height(n->right);
 
+	if(Depth(n->key) > maxDepth){
+		maxDepth = Depth(n->key);
+	}	
+
+	//cout << "MAX DEPTH = " << maxDepth << endl;
+
+
+	if(n->parent == sentinel){
+		int temp = maxDepth;
+		maxDepth = 0;
+		return temp;
+	}
+	return 0;
 }
-//Helper for Ordered_Keys()
-void BSTree::make_key_vector(const BSTNode *n, vector <void *> &v) const{
+
+void BSTree::make_key_vector(const BSTNode *n, vector<string> &v) const{
 	(void) n;
 	(void) v;
 
-	//if(n == sentinel) return;
-	//make_val_vector(n->left, v);
-	//v.push_back(n->val);
-	//make_val_vector(n->right, v);
-
 	if(n == sentinel) return;
+
 	make_key_vector(n->left, v);
 	v.push_back(n->key);
 	make_key_vector(n->right, v);
 }
-//Helper for copy constructor and assignment overload
-BSTNode BSTree::*make_balanced_tree(const vector <string> &sorted_keys, const vector<void *> &vals, size_t first_index, size_t num_indices) const{
+
+BSTNode *BSTree::make_balanced_tree(const vector<string> &sorted_keys, 
+		const vector<void *> &vals,
+		size_t first_index,
+		size_t num_indices) const
+{
 	(void) sorted_keys;
 	(void) vals;
 	(void) first_index;
 	(void) num_indices;
+	static size_t c = 0;
+	static int i = 0;
+	static set <string> used;
+	set <string>::iterator it;
+	
+	//individually create nodes & connect manually
+	BSTNode *parent;
+	BSTNode *n;
+	string key;
+	void * val;
 
-	//be recursive AND create tree in postorder manner
+	//parent = sentinel;
+	//n = sentinel->right;
+	
 
-	//return a root of the tree
+	if(sorted_keys.size() % 2 != 0 && num_indices <= sorted_keys.size() && (first_index+num_indices/2) < sorted_keys.size()){
+		key = sorted_keys.at(first_index + num_indices/2);	//FIX HERE
+		//val = vals.at(first_index + num_indices/2);			//FIX HERE
+	
+		it = used.find(key);
+		if(it == used.end()){
+			val = vals.at(first_index + num_indices/2);
+			used.insert(key);
+		}
+		else
+			key = "";
+	}
+	else{
+		if(num_indices % 2 != 0 && (first_index+num_indices/2) < sorted_keys.size()){
+			key = sorted_keys.at(first_index + num_indices/2);
+			//val = vals.at(first_index + num_indices/2);
+			
+			it = used.find(key);
+			if(it == used.end()){
+				val = vals.at(first_index + num_indices/2);
+				used.insert(key);
+			}
+			else
+				key = "";
+		}
+		else if((num_indices/2 - 1) < sorted_keys.size() && (num_indices/2) < sorted_keys.size()){
+			if(sorted_keys.at(num_indices/2 - 1) > sorted_keys.at(num_indices/2)){
+				key = sorted_keys.at(num_indices/2 - 1);
+				//val = vals.at(num_indices/2 - 1);
+				
+				it = used.find(key);
+				if(it == used.end()){
+					val = vals.at(num_indices/2 - 1);
+					used.insert(key);
+				}
+				else
+					key = "";
+			}
+			else{
+				key = sorted_keys.at(num_indices/2);
+				//val = vals.at(num_indices/2);
+				
+				it = used.find(key);
+				if(it == used.end()){
+					val = vals.at(num_indices/2);
+					used.insert(key);
+				}
+				else
+					key = "";
+			}
+		}
+	}
+	
+	cout << "KEY=" << key << endl;
 
-	return NULL;
+
+
+
+	if(key != ""){
+		parent = sentinel;
+		n = sentinel->right;
+	
+
+	//Find where key should go.
+	while(n != sentinel){
+		parent = n;
+		n = (key < n->key) ? n->left : n->right;
+	}
+
+
+	//Create new node and hook it in
+	n = new BSTNode;
+	n->key = key;		
+	n->val = val;			
+	n->parent = parent;
+	n->left = sentinel;
+	n->right = sentinel;
+
+	
+	//Use correct pointer in parent to point to new node
+	if (parent == sentinel) 
+		sentinel->right = n;
+	else if (key < parent->key)	
+		parent->left = n;
+	else 
+		parent->right = n;
+
+
+	c++;
+	i = 0;
+	}
+	else 
+		i++;
+	
+
+
+
+	//if(c != sorted_keys.size() && num_indices/2 != 0){
+	if(c != sorted_keys.size()){
+	//	cout << "first_index=" << first_index << "		num_indices=" << num_indices << endl;
+
+		//if(num_indices/2 != 0){
+		if(sorted_keys.size() % 2 != 0 && num_indices/2 != 0){
+			make_balanced_tree(sorted_keys, vals, first_index, num_indices/2);
+			make_balanced_tree(sorted_keys, vals, first_index + num_indices/2 + 1, num_indices/2);
+		}
+		else if(sorted_keys.size() % 2 == 0 && num_indices/2 != 0){
+			make_balanced_tree(sorted_keys, vals, first_index, num_indices/2);
+
+
+	//		cout << "\nAFTER	first_index=" << first_index << "      num_indices=" << num_indices << endl;
+			
+			
+			make_balanced_tree(sorted_keys, vals, first_index, num_indices + c + i);
+		}
+	}
+
+
+	//cout << "first_index=" << first_index << "		num_indices=" << num_indices << endl;
+	return n;
 }
-
